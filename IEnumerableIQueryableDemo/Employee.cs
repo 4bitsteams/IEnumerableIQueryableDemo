de0fa472490717199973412a3882e451fd77c513
md.rubel <md.rubel@HOAP2406101764A>
2021-11-21 02:04:15 +0600

    IEnumerable VS IQueryable (in .NET Core)

---
+++ IEnumerableIQueryableDemo/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace IEnumerableIQueryableDemo
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+    }
+}
--- IEnumerableIQueryableDemo/Program.cs
+++ IEnumerableIQueryableDemo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IEnumerableIQueryableDemo
 {
@@ -6,7 +8,39 @@ namespace IEnumerableIQueryableDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var datasource = @"(localdb)\MSSQLLocalDB";
+            var database = "Practice";
+
+            string connString = @"Data Source=" + datasource + ";Initial Catalog="
+                        + database + ";Persist Security Info=False;";
+            var context = new EmployeeContext(connString);
+
+
+            IEnumerable<Employee> _Employee = context.Employees.Where(x => x.Id > 0);
+            _Employee = _Employee.Take(1);
+
+            foreach (var item in _Employee)
+            {
+                Console.WriteLine(item.Id);
+                Console.WriteLine(item.FirstName);
+                Console.WriteLine(item.LastName);
+                Console.WriteLine(item.Address);
+            }
+            Console.WriteLine("//////////////////////////////////////////");
+
+            IQueryable<Employee> Employee = context.Employees.Where(x => x.Id > 0);
+            Employee = Employee.Take(1);
+
+            foreach (var item in Employee)
+            {
+                Console.WriteLine(item.Id);
+                Console.WriteLine(item.FirstName);
+                Console.WriteLine(item.LastName);
+                Console.WriteLine(item.Address);
+            }
+
+            Console.ReadKey();
         }
     }
 }
